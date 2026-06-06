c9a7878fe725138daf203dafd1518b8a666e3f29
Lalex3 <kosel.leonora03.04@gmail.com>
2026-06-07 00:08:13 +0200

    healthbar screen + script

---
+++ progress_bar.gd
@@ -0,0 +1,15 @@
+extends TextureProgressBar
+
+var health_bar = 5
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func update():
+	value = 5
