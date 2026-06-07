db6887d24d681f56db8dcc10e0f96e651bc51ed2
fio <birb@the-oronco.com>
2026-06-07 15:52:55 +0200

    fixed attack bug and updated background

--- entities/chef/chef.gd
+++ entities/chef/chef.gd
@@ -10,26 +10,28 @@ extends Node2D
 @onready var l_cooldown: Timer = %LCooldown
 @onready var r_cooldown: Timer = %RCooldown
 
-var attacking_r := false
-var attacking_l := false
+var attack_r: Attack
+var attack_l: Attack
 
 var rage := 1.
+
+var _do_attack := false
 
 func _ready() -> void:
 	randomize()
 	l_cooldown.wait_time = attack_cooldown
-	l_cooldown.timeout.connect(func(): attacking_l = false)
+	l_cooldown.timeout.connect(func(): attack_l = null)
 	r_cooldown.wait_time = attack_cooldown
-	r_cooldown.timeout.connect(func(): attacking_r = false)
+	r_cooldown.timeout.connect(func(): attack_r = null)
+	get_tree().create_timer(1.).timeout.connect(func(): _do_attack = true)
 
 func _physics_process(delta: float) -> void:
-	if !attacking_l:
-		attacking_l = true
-		var rand_attack: Attack = left_attacks.pick_random()
-		rand_attack.attack(rage * randf_range(.9, 1.1))
-		rand_attack.attack_finished.connect(l_cooldown.start, CONNECT_ONE_SHOT)
-	if !attacking_r:
-		attacking_r = true
-		var rand_attack: Attack = right_attacks.pick_random()
-		rand_attack.attack(rage * randf_range(.9, 1.1))
-		rand_attack.attack_finished.connect(r_cooldown.start, CONNECT_ONE_SHOT)
+	if _do_attack:
+		if !attack_l:
+			attack_l = left_attacks.pick_random()
+			attack_l.attack(rage * randf_range(.9, 1.1))
+			attack_l.attack_finished.connect(l_cooldown.start, CONNECT_ONE_SHOT)
+		if !attack_r:
+			attack_r = right_attacks.pick_random()
+			attack_r.attack(rage * randf_range(.9, 1.1))
+			attack_r.attack_finished.connect(r_cooldown.start, CONNECT_ONE_SHOT)
