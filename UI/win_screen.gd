9097a236ad924b10aaea75b7e3d5acd7209c88c2
Lalex3 <kosel.leonora03.04@gmail.com>
2026-06-07 15:41:01 +0200

    winScreen change

--- UI/win_screen.gd
+++ UI/win_screen.gd
@@ -12,7 +12,7 @@ func _process(_delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/kitchen.tscn")
+	get_tree().change_scene_to_file("res://UI/start_screen.tscn")
 
 
 func _on_exit_pressed() -> void:
